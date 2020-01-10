4a747b5ab179eca4a00d1751cf253ce8eb04049c
alejocruzrcc <alejocruzzz@gmail.com>
2020-01-10 03:27:55 -0500

    agrega has many migracion en modelo proteccion

--- app/models/sivel2_sjr/proteccion.rb
+++ app/models/sivel2_sjr/proteccion.rb
@@ -5,5 +5,8 @@ module Sivel2Sjr
   
   	has_many :casosjr, class_name: "Sivel2Sjr::Casosjr", 
       foreign_key: "id_proteccion", validate: true
+  	
+        has_many :migracion, class_name: "Sivel2Sjr::Migracion", 
+      foreign_key: "proteccion_id", validate: true
   end
 end
